231b75099ec86a99c7116aab9a71ed0840a0751e
Vitas1968 <dor1968@mail.ru>
2019-08-01 22:16:20 +0700

    реализация класса

--- src/Client/Controller.java
+++ src/Client/Controller.java
@@ -4,6 +4,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Controller {
     @FXML
     TextArea textArea;
@@ -12,11 +19,54 @@ public class Controller {
     @FXML
     TextField textField;
 
+    Socket socket;
+    DataInputStream in;
+    DataOutputStream out;
+
+    final String IP_ADRESS = "localhost";
+    final int PORT = 8189;
+
+
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            socket = new Socket(IP_ADRESS, PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            String str = in.readUTF();
+                            textArea.appendText(str + "\n");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
     public void sendMsg() {
-        textArea.appendText(textField.getText() + "\n");
-        textField.clear();
-        textField.requestFocus();
+        try {
+            out.writeUTF(textField.getText());
+            textField.clear();
+            textField.requestFocus();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
