670bf738e096de71a47dd1b688927cd68a6577d6
Vitas1968 <dor1968@mail.ru>
2019-08-03 16:46:14 +0700

    несущественно

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -7,16 +7,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
+        System.out.println("Создание клиента");
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("My Chat");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
