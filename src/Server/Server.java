aecf691d3ffd546156d81d57af81acd3248a4fbb
Vitas1968 <dor1968@mail.ru>
2019-08-04 13:22:41 +0700

    добавил формирование имени клиента

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -8,20 +8,31 @@ import java.util.Vector;
 public class Server
 {
     private Vector<ClientHandler> clients;
+    int count=0;
+    private String nameClient= "Client";
+
+    public Vector getClients()
+    {
+        return clients;
+    }
 
     public Server() {
         clients = new Vector<>();
         ServerSocket server = null;
         Socket socket = null;
 
+
+
         try {
-            server = new ServerSocket(8189);
+            server = new ServerSocket(8086);
             System.out.println("Сервер запущен!");
 
             while (true) {
                 socket = server.accept();
-                clients.add(new ClientHandler(this, socket));
-                System.out.println("Клиент подключился");
+                clients.add(new ClientHandler(this, socket, nameClient+count));
+
+                System.out.println("Клиент " +clients.get(count).getNameClient()+ " подключился");
+                count++;
             }
 
         } catch (IOException e) {
