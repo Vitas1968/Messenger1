af210c3b51b82ea81706e2f90d4da88e9a612b8f
Vitas1968 <dor1968@mail.ru>
2019-09-08 11:16:05 +0700

    тестовый класс с реализацией тестовых методов с разными набрами данных

--- src/Lesson6/ConversArrayTest.java
+++ src/Lesson6/ConversArrayTest.java
@@ -1,6 +1,61 @@
+package Lesson6;
+
 import static org.junit.Assert.*;
 
 public class ConversArrayTest
 {
 
+    @org.junit.Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @org.junit.Test
+    public void conversToArray()
+    {
+        ConversArray conversArray=new ConversArray();
+        int [] arr=conversArray.conversToArray(conversArray.arr);
+        int [] arrTest={56,21,44};
+        assertArrayEquals(arrTest,arr);
+
+    }
+    @org.junit.Test
+    public void conversToArray1()
+    {
+        ConversArray conversArray=new ConversArray();
+        int [] arr=conversArray.conversToArray(conversArray.arr1);
+        int [] arrTest={55,25,45};
+        assertArrayEquals(arrTest,arr);
+
+    }
+    @org.junit.Test
+    public void conversToArray2()
+    {
+        ConversArray conversArray=new ConversArray();
+        int [] arr=conversArray.conversToArray(conversArray.arr2);
+        int [] arrTest={65,35,75};
+        assertArrayEquals(arrTest,arr);
+
+    }
+
+    @org.junit.Test
+    public void checked()
+    {
+        ConversArray conversArray=new ConversArray();
+        assertTrue(conversArray.checked(conversArray.arr));
+
+    }
+
+    @org.junit.Test
+    public void checked1()
+    {
+        ConversArray conversArray=new ConversArray();
+        assertFalse(conversArray.checked(conversArray.arr1));
+
+    }
 }
